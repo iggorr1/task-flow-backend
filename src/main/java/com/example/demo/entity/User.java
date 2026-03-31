160cba1249a96e9e2d77309171482b6d5a9ca32d
iggorr1 <48808520+iggorr1@users.noreply.github.com>
2026-03-31 17:44:05 +0300

    add Controller, User, UserRepository

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
+    public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public User() {}
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
