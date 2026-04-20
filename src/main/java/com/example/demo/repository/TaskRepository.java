81fd08b8582ec1542d0d8383327adc554930d7e8
iggorr1 <igorkov05@gmail.com>
2026-04-20 18:08:34 +0300

    add completed

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findByUser(User user, Pageable pageable);
+    Page<Task> findByUserAndCompleted(User user, boolean completed, Pageable pageable);
 }
